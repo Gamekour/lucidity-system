edfd06eb23dc57dd7ac8d3f68cf68aa7c79e9118
Gamekour <49810363+Gamekour@users.noreply.github.com>
2026-09-02 20:54:16 -0400

    Revert "better camera"
    This reverts commit c880db9aa0415d25774a2dbe9103744c1a92c28c.

--- addons/lucidity_system/camera_controller.gd
+++ addons/lucidity_system/camera_controller.gd
@@ -7,6 +7,7 @@ class_name CameraController
 @export_range(-90.0, 90.0, 0.5, "radians_as_degrees") var max_camera_pitch : float = deg_to_rad(80.0)
 @export var cam_distance_max := 4.0
 @export var cam_distance_min := 0.0
+@export var camera_tilt_smoothing : float = 10.0
 @export var fp_deadzone : float = 0.05
 
 var target : Node3D
@@ -15,15 +16,12 @@ var has_focus_origin : bool = false
 
 var camera_pitch : float = 0.0
 var target_angle_horizontal : float = 0.0
-
-var _last_target_yaw : float = 0.0
-var _has_last_target_yaw : bool = false
+var camera_up_dir : Vector3 = Vector3.UP
 
 func set_target(new_target: Node3D) -> void:
 	target = new_target
 	focus_origin = null
 	has_focus_origin = false
-	_has_last_target_yaw = false
 	if is_instance_valid(target):
 		var origin_node := target.find_child("focus_origin")
 		if origin_node is Node3D:
@@ -50,28 +48,43 @@ func _process(delta: float) -> void:
 		return
 	if has_focus_origin:
 		camera_pitch = clampf(camera_pitch, min_camera_pitch, max_camera_pitch)
-
 	var up_dir := _get_target_up_dir().normalized()
-	var target_yaw := _get_target_yaw(up_dir)
-	if _has_last_target_yaw:
-		var yaw_delta := wrapf(target_yaw - _last_target_yaw, -PI, PI)
-		target_angle_horizontal = wrapf(target_angle_horizontal - yaw_delta, -PI, PI)
-	_last_target_yaw = target_yaw
-	_has_last_target_yaw = true
-
-	var yaw_offset_basis := Basis(Vector3.UP, target_angle_horizontal)
-	var pitch_offset_basis := Basis(Vector3.RIGHT, camera_pitch)
+	var tilt_t : float = 1.0 - exp(-camera_tilt_smoothing * delta)
+	camera_up_dir = camera_up_dir.normalized()
+	camera_up_dir = _safe_slerp_up(camera_up_dir, up_dir, tilt_t)
+	var tilt_basis := _get_tilt_basis(camera_up_dir)
+	var yaw_basis := Basis(Vector3.UP, target_angle_horizontal)
+	var pitch_basis := Basis(Vector3.RIGHT, camera_pitch)
 	var pivot_position := focus_origin.global_position if has_focus_origin else target.global_position
 	global_position = pivot_position
-	global_basis = target.global_basis.orthonormalized() * yaw_offset_basis * pitch_offset_basis
+	global_basis = tilt_basis * yaw_basis * pitch_basis
 
 func _get_target_up_dir() -> Vector3:
 	if target is PhysicsPlayerController:
 		return (target as PhysicsPlayerController).current_up_dir
 	return Vector3.UP
 
-func _get_target_yaw(up_dir: Vector3) -> float:
-	var q := target.global_basis.orthonormalized().get_rotation_quaternion()
-	var qv := Vector3(q.x, q.y, q.z)
-	var proj := qv.dot(up_dir)
-	return wrapf(2.0 * atan2(proj, q.w), -PI, PI)
+func _get_tilt_basis(up_dir: Vector3) -> Basis:
+	var axis := Vector3.UP.cross(up_dir)
+	var axis_length := axis.length()
+	if axis_length < 0.0001:
+		if Vector3.UP.dot(up_dir) < 0.0:
+			return Basis(Vector3.RIGHT, PI)
+		return Basis.IDENTITY
+	axis /= axis_length
+	var angle := Vector3.UP.angle_to(up_dir)
+	return Basis(axis, angle)
+
+func _safe_slerp_up(from: Vector3, to: Vector3, weight: float) -> Vector3:
+	from = from.normalized()
+	to = to.normalized()
+	var dot := clampf(from.dot(to), -1.0, 1.0)
+	if dot > 0.9995:
+		return from.lerp(to, weight).normalized()
+	if dot < -0.9995:
+		var arbitrary := Vector3.RIGHT if abs(from.x) < 0.9 else Vector3.UP
+		var axis := from.cross(arbitrary).normalized()
+		return from.rotated(axis, PI * weight).normalized()
+	var theta := acos(dot) * weight
+	var relative := (to - from * dot).normalized()
+	return (from * cos(theta) + relative * sin(theta)).normalized()
