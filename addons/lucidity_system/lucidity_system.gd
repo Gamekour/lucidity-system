60413c3edf7ba48afcc72f8b679b30190d8e1f85
Gamekour <49810363+Gamekour@users.noreply.github.com>
2026-09-06 16:59:19 -0400

    plugin updater support

--- addons/lucidity_system/lucidity_system.gd
+++ addons/lucidity_system/lucidity_system.gd
@@ -11,12 +11,17 @@ func _disable_plugin() -> void:
 	# Remove autoloads here.
 	pass
 
-
 func _enter_tree() -> void:
-	# Initialization of the plugin goes here.
-	pass
-
+	var plugin_repos:Dictionary = ProjectSettings.get_setting("plugin_updater/plugins", {})
+	plugin_repos[get_plugin_path()] = "https://github.com/{USERNAME}/{REPO_NAME}"
+	ProjectSettings.set_setting("plugin_updater/plugins", plugin_repos)
+	ProjectSettings.save()
 
 func _exit_tree() -> void:
-	# Clean-up of the plugin goes here.
-	pass
+	var plugin_repos:Dictionary = ProjectSettings.get_setting("plugin_updater/plugins", {})
+	plugin_repos.erase(get_plugin_path())
+	ProjectSettings.set_setting("plugin_updater/plugins", plugin_repos)
+	ProjectSettings.save()
+	
+func get_plugin_path() -> String:
+	return get_script().resource_path.get_base_dir() + "/"
