49577dec34a9f6e95becaf50ef10976ed1746100
Gamekour <49810363+Gamekour@users.noreply.github.com>
2026-09-25 18:26:52 -0400

    fov smoothing

--- addons/lucidity_system/camera_motion_features.gd
+++ addons/lucidity_system/camera_motion_features.gd
@@ -4,6 +4,7 @@ extends Camera3D
 @export var fov_speed_min := 3.0
 @export var fov_speed_max := 12.0
 @export var max_fov := 120.0
+@export var fov_smoothing := 8.0
 
 var last_position := Vector3.ZERO
 var base_fov := 0.0
@@ -19,6 +20,8 @@ func _physics_process(delta: float) -> void:
 	basis = Basis(Vector3.FORWARD, -local_velocity.x * PI * tilt_strength)
 	
 	var new_fov = remap(velocity.length(), fov_speed_min * delta / 20, fov_speed_max * delta / 20, base_fov, max_fov)
-	fov = clampf(new_fov, base_fov, max_fov)
+	new_fov = clampf(new_fov, base_fov, max_fov)
+	var t := 1.0 - exp(-fov_smoothing * delta)
+	fov = lerp(fov, new_fov, t)
 	
 	last_position = parent.global_position
